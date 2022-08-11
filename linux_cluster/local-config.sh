dbb67bcc10312afb915372c581894aec59a232dc
Elliott <ezw193@gmail.com>
2022-08-10 22:50:26 -0400

    removed unnecessary files

--- linux_cluster/local-config.sh
+++ linux_cluster/local-config.sh
@@ -1,5 +1,5 @@
 echo "Configuring Linux cluster..."
 sudo docker network create --driver bridge linux_swarm
-sudo docker compose --file linux_cluster-compose.yml up -d
+sudo docker compose --file user_config.yml up -d
 echo "Linux cluster configured!"
 
